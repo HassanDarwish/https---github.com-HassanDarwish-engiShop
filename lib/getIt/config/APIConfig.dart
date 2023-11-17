2fc32f6c07c552e2e906cfe39aaf2cffe5816a37
Hassan Ali Hassan <hhassan@ntgclairty.com>
2023-11-17 15:25:03 +0200

    apply ssl to APIConfig.dart with 10 time request as sloution for dilay

--- lib/getIt/config/APIConfig.dart
+++ lib/getIt/config/APIConfig.dart
@@ -15,53 +15,58 @@ abstract class API_Config {
 class API_Config_Implementation extends API_Config {
 
 
-
-  Future<bool> isInternet () async {
-    bool internet=true;
+  Future<bool> isInternet() async {
+    bool internet = true;
     try {
       final result = await InternetAddress.lookup('www.jerma.net');
       if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-       internet=true;
+        internet = true;
       }
     } on SocketException catch (_) {
-      internet=false;
+      internet = false;
     }
 
     return internet;
   }
 
 
-
   Future<Config> getConfig() async {
     // TODO: implement getProductByCategory
-
     // Read the certificate content from the file
+    int maxRetries = 10;
+    int currentRetry = 0;
+
     final certificateAsset = await rootBundle.load('assets/certificate.pem');
-    final certificateContent = utf8.decode(certificateAsset.buffer.asUint8List());
+    final certificateContent = utf8.decode(
+        certificateAsset.buffer.asUint8List());
 
     // Convert certificate content to bytes
     List<int> certificateBytes = utf8.encode(certificateContent);
-     // Create a SecurityContext and add the certificate to it
+    // Create a SecurityContext and add the certificate to it
     SecurityContext securityContext = SecurityContext.defaultContext;
     securityContext.setTrustedCertificatesBytes(certificateBytes);
 
-
-
-
-    // Make the HTTP request with the custom security context
-    var response = await http.get(
-      Uri.parse('https://www.jerma.net/Engi/api/config.php'), // Use 'https' instead of 'http'
-      headers: {"Content-Type": "application/json"},
-    ).timeout(Duration(seconds: 10));
-
-    // Check the response status
-    if (response.statusCode == 200) {
-      // If the server returns a 200 OK response, parse the JSON and return the config
-      config= Config.fromJson(jsonDecode(response.body));
-      return config;
-    } else {
-      // If the server returns an error response, throw an exception with the error message
-      throw Exception('Failed to load config: ${response.statusCode}');
+    while (currentRetry < maxRetries) {
+      try {
+        var response = await http.get(
+          Uri.parse('https://www.jerma.net/Engi/api/config.php'),
+          // Use 'https' instead of 'http'
+          headers: {"Content-Type": "application/json"},
+        ).timeout(Duration(seconds: 4));
+        // Make HTTP request
+        if (response.statusCode == 200) {
+          // If the server returns a 200 OK response, parse the JSON and return the config
+          config = Config.fromJson(jsonDecode(response.body));
+          break;
+        }
+ // Break the loop if the request is successful
+      } catch (error) {
+        // Handle error
+        currentRetry++;
+      }
     }
+    // Make the HTTP request with the custom security context
+        return config;
+  } //while
+
   }
-}
