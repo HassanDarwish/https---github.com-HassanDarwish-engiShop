eac2d8b245b84a89993c6255325646ca030c3f3e
Hassan Ali Hassan <hhassan@ntgclairty.com>
2023-11-18 17:24:55 +0200

    apply ssl to to get productbyID fix SSL self Signed cer and cashedNetworkImage

--- lib/getIt/config/APIConfig.dart
+++ lib/getIt/config/APIConfig.dart
@@ -6,14 +6,15 @@ import 'package:GiorgiaShop/Helper/SSLLoder.dart';
 import 'package:GiorgiaShop/pojo/config.dart';
 import 'package:http/io_client.dart';
 import 'package:flutter/services.dart';
- import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
+
 abstract class API_Config {
   Future<Config> getConfig();
   Future<bool> isInternet();
   late Config config;
 }
 
-class API_Config_Implementation extends API_Config with SSLLoader {
+class API_Config_Implementation extends API_Config  {
 
 
   Future<bool> isInternet() async {
@@ -36,20 +37,28 @@ class API_Config_Implementation extends API_Config with SSLLoader {
     // Read the certificate content from the file
     int maxRetries = 20;
     int currentRetry = 0;
+/*
+  var request = Uri.parse('https://www.jerma.net/Engi/api/config.php');
+  var headers = {'Content-Type': 'application/json'};
+  var body = '{"message": "Hello from Flutter!"}';
 
-    ConfigSSLLoader();
+  var response = await client.post(request, headers: headers, body: body);
+ */
+    HttpClient client = HttpClient()..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
 
     while (currentRetry < maxRetries) {
       try {
-        var response = await http.get(
-          Uri.parse('https://www.jerma.net/Engi/api/config.php'),
-          // Use 'https' instead of 'http'
-          headers: {"Content-Type": "application/json"},
-        ).timeout(Duration(seconds: 4));
-        // Make HTTP request
-        if (response.statusCode == 200) {
+        var request = await client.getUrl(
+          Uri.parse('https://www.jerma.net/Engi/api/config.php')
+        ).timeout(Duration(seconds: 12));
+        // Set headers for the request
+        request.headers.set('Content-Type', 'application/json');
+        //request.headers.set('Authorization', 'Bearer YourAccessToken');
+        HttpClientResponse response = await request.close();
+         if (response.statusCode == 200) {
           // If the server returns a 200 OK response, parse the JSON and return the config
-          config = Config.fromJson(jsonDecode(response.body));
+           String responseBody = await response.transform(utf8.decoder).join();
+          config = Config.fromJson(jsonDecode(responseBody));
           break;
         }
  // Break the loop if the request is successful
--- lib/getIt/woocommecre/APICustomWooCommerce.dart
+++ lib/getIt/woocommecre/APICustomWooCommerce.dart
@@ -1,7 +1,8 @@
 import 'dart:collection';
 import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
-
+import 'package:http/http.dart' show IOClient;
 import 'package:flutter_wp_woocommerce/models/order.dart' as orderr;
 import 'package:GiorgiaShop/pojo/favorit/Favorit.dart';
 import 'package:GiorgiaShop/pojo/order/lineItems.dart';
@@ -16,7 +17,6 @@ import '../../pojo/customer/customers.dart';
 import '../../pojo/tracking/TrackingOrder.dart';
 import '../config/APIConfig.dart';
 
-import 'package:GiorgiaShop/Helper/SSLLoder.dart';
 
 GetIt getIt = GetIt.instance;
 
@@ -35,10 +35,11 @@ abstract class APICustomWooCommerce {
   Future<bool> addToFavorite(String userID,String itemId);
 
   Future<List<TrackingOrder>> getOrderByUserId(String userId);
+  HttpClient client = HttpClient()..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
 
 }
 
-class APICustomWooCommerce_Implementation extends APICustomWooCommerce  with SSLLoader{
+class APICustomWooCommerce_Implementation extends APICustomWooCommerce{
   //String consumerKey ="";// "ck_314081f754984f4ec9a55e8ca4c2171bd071ea56";
   //String consumerSecret ="";// "cs_8ae1b05d30d722960f3d65136dd82ee0433417cf";
   Future<List<TrackingOrder>> getOrderByUserId(String userId)async{
@@ -265,28 +266,40 @@ Future<List<Favorit>> ListFavorit(userId) async{
     }
     return coupon;
   }
+  Duration calculateTimeout(int currentRetry) {
+    // Calculate timeout based on retry attempt, e.g., exponential backoff
+    int baseTimeout = 5; // seconds
+    int maxTimeout = 60; // seconds
 
+    int calculatedTimeout = baseTimeout * (2 ^ currentRetry);
+    return Duration(seconds: calculatedTimeout.clamp(0, maxTimeout));
+  }
   @override
   Future<products> getProductByCategory(String catId) async {
     late products product_List;
     // TODO: implement getProductByCategory
-    int maxRetries = 20;
+    int maxRetries = 5;
     int currentRetry = 0;
 
-    WooSSLLoader();
     while (currentRetry < maxRetries) {
       try {
-        var response = await http.get(
+
+
+        var request = await client.getUrl(
             Uri.parse(getOAuthURL(
                 "GET",
                 'https://engy.jerma.net/wp-json/wc/v3/products?category=' +
                     catId +
                     "&status=publish")),
-            headers: {"Content-Type": "Application/json"}).timeout(
-            Duration(seconds: 7));
-//List<dynamic> list = jsonDecode(jsonString);
+        ).timeout(
+            Duration(seconds: 12));
+        request.headers.set('Content-Type', 'application/json');
+        //request.headers.set('Authorization', 'Bearer YourAccessToken');
+        HttpClientResponse response = await request.close();
         if (response.statusCode == 200) {
-          product_List = products.fromJson(response.body);
+          // If the server returns a 200 OK response, parse the JSON and return the config
+          String responseBody = await response.transform(utf8.decoder).join();
+          product_List = products.fromJson(responseBody);
         break;
         }
       } catch (error) {
@@ -294,7 +307,7 @@ Future<List<Favorit>> ListFavorit(userId) async{
         currentRetry++;
       }
     }
-    print("www");
+
     return product_List;
   }
 
