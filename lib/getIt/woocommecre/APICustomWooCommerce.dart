14e7227b4603bb7dab0c08f3bb36e802cf76d82e
HassanAli <hassanaly@gmail.com>
2023-11-20 12:11:27 +0200

    SSL works at [ createOrder2 ]

--- lib/getIt/woocommecre/APICustomWooCommerce.dart
+++ lib/getIt/woocommecre/APICustomWooCommerce.dart
@@ -2,11 +2,11 @@ import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
+import 'package:GiorgiaShop/pojo/order/shipping_lines.dart';
 import 'package:http/http.dart' show IOClient;
 import 'package:flutter_wp_woocommerce/models/order.dart' as orderr;
 import 'package:GiorgiaShop/pojo/favorit/Favorit.dart';
-import 'package:GiorgiaShop/pojo/order/lineItems.dart';
-import 'package:flutter_wp_woocommerce/models/customer.dart';
+ import 'package:flutter_wp_woocommerce/models/customer.dart';
 import 'package:GiorgiaShop/pojo/coupon/coupons.dart';
 import 'package:GiorgiaShop/pojo/products.dart';
 import 'package:crypto/crypto.dart' as crypto;
@@ -16,6 +16,7 @@ import 'package:http/http.dart' as http;
 import '../../pojo/customer/customers.dart';
 import '../../pojo/tracking/TrackingOrder.dart';
 import '../config/APIConfig.dart';
+import 'APICustomWooCommerce.dart';
 
 
 GetIt getIt = GetIt.instance;
@@ -218,32 +219,56 @@ Future<List<Favorit>> ListFavorit(userId) async{
     shippingFees1=finalExpanses.toString();
 
     Map<String,int> lineItemsMap= {};
-    List<Map<String, dynamic>> lineItems=List.empty(growable: true);
-    List<Map<String, dynamic>> listmetaMap=List.empty(growable: true);
+    //List<Map<String, dynamic>> lineItems=List.empty(growable: true);
+    //List<Map<String, dynamic>> listmetaMap=List.empty(growable: true);
+    List<LineItem> lineItems=List.empty(growable: true);
+    List<meta_data> listmetaMap=List.empty(growable: true);
 
-
+    int quantity=0;
     for (var product in products) {
-      if(lineItemsMap.isNotEmpty)
-        lineItemsMap={};
+      LineItem lineItemsMap;
 
-           lineItemsMap['"product_id"']=int.parse(product.id);
+           //lineItemsMap['"product_id"']=int.parse(product.id);
 
           if(product.SelectedAttribute!.length>0) {
-            lineItemsMap['"quantity"'] =itemMap[product.identify_value.toString()];
+            //lineItemsMap['"quantity"'] =itemMap[product.identify_value.toString()];
+            int quantity=itemMap[product.identify_value.toString()];
             for (var attr in product.attributes!) {
-              Map<String, dynamic> meta_data={};
-              meta_data['"key"']='"${attr.name+'_'+getRandomString()}"';
-              meta_data['"value"']='"${product.SelectedAttribute![attr.name]!}"';
-              lineItemsMap['"quantity"']=1;
-              listmetaMap.add(meta_data);
+              //Map<String, dynamic> meta_data={};
+              meta_data metaData=new meta_data(value: product.SelectedAttribute![attr.name]!,key: attr.name+'_'+getRandomString());
+            //  meta_data['"key"']='"${attr.name+'_'+getRandomString()}"';
+            //  meta_data['"value"']='"${product.SelectedAttribute![attr.name]!}"';
+              //lineItemsMap['"quantity"']=1;
+              quantity=1;
+              listmetaMap.add(metaData);
 
             }
           }else{
-            lineItemsMap['"quantity"'] = itemMap[product.id];
+            //lineItemsMap['"quantity"'] = itemMap[product.id];
+            quantity=itemMap[product.id];
           }
+      lineItemsMap=new LineItem(quantity: quantity,product_id: int.parse(product.id));
+
            lineItems.add(lineItemsMap);
     }
-    dynamic C={
+    Billing Bi=new Billing(email: email,last_name:"" ,postcode:"" ,city: addressList["city"],state: addressList["state"],
+        country:addressList["country"] ,
+        first_name: displayName,phone:addressList["mobile"] ,address_1: utf8.encode(addressList["address"]).toString());
+
+
+    Shipping Shi=new Shipping(last_name:"" ,postcode:"" ,city: addressList["city"],state: addressList["state"],
+        country:addressList["country"] ,
+        first_name: displayName ,address_1: utf8.encode(addressList["address"]).toString());
+
+    ShippingLine Shipping_Line=new ShippingLine(methodId: "flat_rate", method_title:"Flat Rate",total:shippingFees1);
+    List<ShippingLine> ShipingList=List<ShippingLine>.empty(growable: true);
+    ShipingList.add(Shipping_Line);
+
+    C h=new C(payment_method: "COD", payment_method_title: "CASH On Delever", set_paid: "false",
+        total: double.parse(cartFinalPrice), lineItems: lineItems, listmetaMap: listmetaMap, billing: Bi, shipping: Shi,
+        shipping_lines: ShipingList);
+/*
+    Object C={
       '"payment_method"': '"COD"',
       '"payment_method_title"': '"CASH On Delever"',
       '"set_paid"': '"false"',
@@ -280,23 +305,46 @@ Future<List<Favorit>> ListFavorit(userId) async{
 
       //"${listmetaMap}",
     };
+*/
 
+    String json = jsonEncode(h.toJson());
 
-    var response = await http.post(
-        Uri.parse(getOAuthURL(
-            "POST",
-            'http://engy.jerma.net/wp-json/wc/v3/orders' )),
-        headers: {"Content-Type": "Application/json"},
-        body: C.toString()
+    try {
+      // TODO: implement getProductByCategory
 
-    );
+      var request = await HttpClient().postUrl(
+        Uri.parse(getOAuthURL("POST", 'https://engy.jerma.net/wp-json/wc/v3/orders')),
+      );
+      request.headers.set('Content-Type', 'application/json');
+      request.write(json);
+      // Send the request
+      HttpClientResponse response = await request.close();
 
-    if (response.statusCode== 201) {
-      return true;
-    } else {
-      return false;
-      // The order failed to be created.
+      // Check the response status code
+      if (response.statusCode == 200 || response.statusCode == 201) {
+         return true;
+      } else{
+        return false;
+      }
+ } catch (e) {
+      throw e;
     }
+
+    // var response = await http.post(
+    //     Uri.parse(getOAuthURL(
+    //         "POST",
+    //         'http://engy.jerma.net/wp-json/wc/v3/orders' )),
+    //     headers: {"Content-Type": "Application/json"},
+    //     body: C.toString()
+    //
+    // );
+    //
+    // if (response.statusCode== 201) {
+    //   return true;
+    // } else {
+    //   return false;
+    //   // The order failed to be created.
+    // }
   }
 
 
@@ -411,17 +459,36 @@ Future<List<Favorit>> ListFavorit(userId) async{
     String json = jsonEncode(cust);
     try {
       // TODO: implement getProductByCategory
-      var response = await http.post(
-        Uri.parse(getOAuthURL("PUT",
-            'http://engy.jerma.net/wp-json/wc/v3/customers/${id}')),
-        headers: {"Content-Type": "Application/json"},
-        body: json,
-      );
 
-      List<dynamic> Json = jsonDecode(response.body);
-      !Json.isEmpty
-          ? customer = customers.fromJson(jsonDecode(response.body))
-          : customer = customers(email: "empty");
+      var request = await HttpClient().putUrl(
+        Uri.parse(getOAuthURL("PUT", 'https://engy.jerma.net/wp-json/wc/v3/customers/${id}')),
+      );
+      request.headers.set('Content-Type', 'application/json');
+      request.write(json);
+      // Send the request
+      HttpClientResponse response = await request.close();
+
+      // Check the response status code
+      if (response.statusCode == 200) {
+        String responseBody = await response.transform(utf8.decoder).join();
+        List<dynamic> Json = jsonDecode(responseBody);
+        !Json.isEmpty
+             ? customer = customers.fromJson(jsonDecode(responseBody))
+             : customer = customers(email: "empty");
+      }else{
+        customer = customers(email: "empty");
+      }
+
+      // var response = await http.post(
+      //   Uri.parse(getOAuthURL("PUT",
+      //       'http://engy.jerma.net/wp-json/wc/v3/customers/${id}')),
+      //   headers: {"Content-Type": "Application/json"},
+      //   body: json,
+      // );
+      //  List<dynamic> Json = jsonDecode(response.body);
+      // !Json.isEmpty
+      //     ? customer = customers.fromJson(jsonDecode(response.body))
+      //     : customer = customers(email: "empty");
     } catch (e) {
       throw e;
     }
@@ -544,5 +611,175 @@ class QueryString {
     }
 
     return result;
+  }
+}
+
+class Billing{
+  String first_name="";
+  String last_name="";
+  String address_1="";
+  String city="";
+  String state="";
+  String postcode="";
+  String country="";
+  String email="";
+  String phone="";
+  Billing({
+    required this.first_name,
+    required this.last_name,
+    required this.address_1,
+    required this.city,
+    required this.state,
+    required this.postcode,
+    required this.country,
+    required this.email,
+    required this.phone,
+  });
+  Map<String, dynamic> toJson() {
+    return {
+      'first_name': first_name,
+      'last_name': last_name,
+      'address_1': address_1,
+      'city': city,
+      'state': state,
+      'postcode': postcode,
+      'country': country,
+      'email': email,
+      'phone': phone,
+    };
+  }
+}
+class Shipping{
+  String first_name="";
+  String last_name="";
+  String address_1="";
+  String city="";
+  String state="";
+  String postcode="";
+  String country="";
+  Shipping({
+    required this.first_name,
+    required this.last_name,
+    required this.address_1,
+    required this.city,
+    required this.state,
+    required this.postcode,
+    required this.country,
+  });
+  Map<String, dynamic> toJson() {
+    return {
+      'first_name': first_name,
+      'last_name': last_name,
+      'address_1': address_1,
+      'city': city,
+      'state': state,
+      'postcode': postcode,
+      'country': country,
+    };
+  }
+}
+class ShippingLine {
+  String methodId;
+  String method_title;
+  String total;
+  ShippingLine.empty()
+      : methodId = '',
+        method_title = '',
+        total = "0";
+
+  ShippingLine({
+    required this.methodId,
+    required this.method_title,
+    required this.total,
+  });
+  Map<String, dynamic> toJson() {
+    return {
+      'method_id': methodId,
+      'method_title': method_title,
+      'total': total,
+    };
+  }
+  factory ShippingLine.fromJson(Map<String, dynamic> json) {
+    return ShippingLine(
+      methodId: json['method_id'],
+      method_title: json['method_title'],
+      total: json['total'],
+    );
+  }
+}
+class meta_data{
+  String key;
+  String value;
+  meta_data({required this.key, required this.value});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'key': key,
+      'value': value,
+    };
+  }
+}
+class LineItem {
+  int product_id;
+  int quantity;
+
+  LineItem({
+    required this.product_id,
+    required this.quantity,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'product_id': product_id,
+      'quantity': quantity,
+    };
+  }
+}
+class C {
+  String payment_method="";
+  String payment_method_title="";
+  String set_paid="false";
+  double total=0;
+  List<LineItem> lineItems = List.empty(growable: true);
+  List<meta_data> listmetaMap = List.empty(growable: true);
+  late Billing billing;
+  late Shipping shipping;
+  List<ShippingLine> shipping_lines = [];
+
+  C({
+    required this.payment_method,
+    required this.payment_method_title,
+    required this.set_paid,
+    required this.total,
+    required this.lineItems,
+    required this.listmetaMap,
+    required this.billing,
+    required this.shipping,
+    required this.shipping_lines,
+  });
+
+  C.fromJson(Map<String, dynamic> json) {
+    payment_method = json['payment_method'];
+    payment_method_title = json['payment_method_title'];
+    set_paid = json['set_paid'];
+    total = json['total'];
+    lineItems = json['line_items'];
+    listmetaMap = json['meta_data'];
+    // billing = Billing.fromJson(json['billing']);
+    // shipping = Shipping.fromJson(json['shipping']);
+    shipping_lines = json['shipping_lines'].map((line) => ShippingLine.fromJson(line)).toList();
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'payment_method': payment_method,
+      'payment_method_title': payment_method_title,
+      'set_paid': set_paid,
+      'total': total,
+      'line_items': lineItems,
+      'meta_data': listmetaMap,
+      'billing': billing.toJson(),
+      'shipping': shipping.toJson(),
+      'shipping_lines': shipping_lines.map((line) => line.toJson()).toList(),
+    };
   }
 }
